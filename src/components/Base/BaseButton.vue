f1f7569adc975f1751a6e279dfe476c65006688e
DavidEliauri <eliauri.david@gmail.com>
2024-05-25 14:30:40 +0300

    added base button

---
+++ src/components/Base/BaseButton.vue
@@ -0,0 +1,66 @@
+<script setup lang="ts">
+    import BaseLoading from '@/components/Base/BaseLoading.vue';
+</script>
+
+<template>
+    <!-- On load add .button_loading -->
+    <button class="button">
+        <span class="button__text">
+            <slot />
+        </span>
+
+        <span class="button__loading">
+            <BaseLoading />
+        </span>
+    </button>
+</template>
+
+<style scoped lang="scss">
+    .button {
+        border: 1px solid #002dbf;
+        border-radius: 8px;
+        font-size: 20px;
+        font-weight: 600;
+        background: none;
+        color: #002dbf;
+        fill: #002dbf;
+        padding: 16px 32px;
+        cursor: pointer;
+        transition: all 100ms;
+        position: relative;
+
+        &:hover,
+        &:focus {
+            background-color: #002dbf;
+            color: white;
+            fill: white;
+        }
+
+        &__text {
+            transition: opacity 500ms ease;
+        }
+
+        &__loading {
+            display: block;
+            width: 36px;
+            height: 36px;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            transition: opacity 500ms ease;
+            opacity: 0;
+        }
+
+        &_loading {
+            .button__text {
+                opacity: 0;
+            }
+
+            .button__loading {
+                opacity: 1;
+            }
+        }
+    }
+</style>
+
--- src/components/TheNews.vue
+++ src/components/TheNews.vue
@@ -1,19 +1,34 @@
 <script setup lang="ts">
     import NewsCard from '@/components/NewsCard.vue';
+    import BaseButton from '@/components/Base/BaseButton.vue';
 </script>
 
 <template>
     <div class="news">
-        <NewsCard v-for="n in 6" />
+        <div class="news__items">
+            <NewsCard v-for="n in 6" />
+        </div>
+
+        <div class="news__load-more">
+            <BaseButton>Загрузить еще</BaseButton>
+        </div>
     </div>
 </template>
 
 <style scoped lang="scss">
     .news {
-        display: grid;
-        grid-template-columns: repeat(3, minmax(0, 1fr));
-        row-gap: 64px;
-        column-gap: 48px;
+        &__items {
+            display: grid;
+            grid-template-columns: repeat(3, minmax(0, 1fr));
+            row-gap: 64px;
+            column-gap: 48px;
+        }
+
+        &__load-more {
+            margin-top: 72px;
+            display: flex;
+            justify-content: center;
+        }
     }
 </style>
 
