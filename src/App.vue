abf20b2f939fe41731cceea89536bc758b705748
DavidEliauri <eliauri.david@gmail.com>
2024-05-25 13:10:33 +0300

    completed footer

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,29 @@
 <script setup lang="ts">
     import TheHeader from '@/components/TheHeader.vue';
     import TheHero from '@/components/TheHero.vue';
+    import TheFooter from '@/components/TheFooter.vue';
 </script>
 
 <template>
-    <div>
+    <div class="page">
         <TheHeader />
 
-        <TheHero>Новости</TheHero>
+        <main>
+            <TheHero>Новости</TheHero>
+        </main>
+
+        <TheFooter />
     </div>
 </template>
 
-<style scoped></style>
+<style scoped lang="scss">
+    .page {
+        min-height: 100vh;
+        display: flex;
+        flex-direction: column;
+    }
+
+    main {
+        flex: 1 1 auto;
+    }
+</style>
---
+++ src/components/TheFooter.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts"></script>
+
+<template>
+    <footer class="footer">
+        <div class="container">
+            <img
+                src="@/assets/logo.svg"
+                alt="Logo"
+                class="footer__logo"
+            />
+
+            <div class="footer__copyright">© alpha, 2023</div>
+        </div>
+    </footer>
+</template>
+
+<style scoped lang="scss">
+    .footer {
+        background-color: #f0f6fe;
+        padding: 64px 0;
+
+        &__logo {
+            display: block;
+            width: 115px;
+        }
+
+        &__copyright {
+            margin-top: 16px;
+            color: #81899d;
+        }
+    }
+</style>
+
