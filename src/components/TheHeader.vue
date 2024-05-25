a73418a13db51e89a94dcb468763aab723e35e38
DavidEliauri <eliauri.david@gmail.com>
2024-05-25 12:28:27 +0300

    completed header

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts"></script>
+
+<template>
+    <header class="header">
+        <div class="container">
+            <div class="header__logo">
+                <img
+                    src="@/assets/logo.svg"
+                    class="header__logo-img"
+                />
+            </div>
+        </div>
+    </header>
+</template>
+
+<style lang="scss">
+    .header {
+        padding: 40px 0;
+        box-shadow: 0px 8px 16px 0px #00000014;
+
+        &__logo {
+        }
+    }
+</style>
+
