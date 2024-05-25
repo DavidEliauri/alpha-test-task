3887da0246f6271934dcb315658bba15a5d3fd42
DavidEliauri <eliauri.david@gmail.com>
2024-05-25 13:03:45 +0300

    completed hero component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
     import TheHeader from '@/components/TheHeader.vue';
+    import TheHero from '@/components/TheHero.vue';
 </script>
 
 <template>
     <div>
         <TheHeader />
+
+        <TheHero>Новости</TheHero>
     </div>
 </template>
 
---
+++ src/components/TheHero.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts"></script>
+
+<template>
+    <div class="hero">
+        <div class="hero__container">
+            <div class="container">
+                <h1 class="hero__title">
+                    <slot />
+                </h1>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss">
+    .hero {
+        height: 320px;
+        background-image: url('@/assets/hero-background.jpg');
+        background-size: cover;
+        display: flex;
+        align-items: flex-end;
+
+        &__container {
+            padding: 48px 0;
+        }
+
+        &__title {
+            font-weight: 700;
+            font-size: 40px;
+        }
+    }
+</style>
+
